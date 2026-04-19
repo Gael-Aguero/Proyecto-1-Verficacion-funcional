29896e46ef294bc84ed8585e84ab5fbb95d7d110
Raquel <kendyraquelarias@gmail.com>
2026-04-18 22:40:02 -0600

    Mejora ambiente: Se separa el monitoreo del driver añadiendo instancia de Monitor y se flexibiliza la conexión de mailboxes del Test

--- ambiente.sv
+++ ambiente.sv
@@ -5,6 +5,7 @@
 class ambiente #(parameter width =16, parameter depth = 8);
   // Declaración de los componentes del ambiente
   driver #(.width(width)) driver_inst;
+  monitor #(.width(width)) monitor_inst;
   checker_c #(.width(width),.depth(depth)) checker_inst;
   score_board #(.width(width)) scoreboard_inst;
   agent #(.width(width),.depth(depth)) agent_inst;
@@ -14,43 +15,56 @@ class ambiente #(parameter width =16, parameter depth = 8);
 
   //declaración de los mailboxes
   trans_fifo_mbx agnt_drv_mbx;           //mailbox del agente al driver
-  trans_fifo_mbx drv_chkr_mbx;           //mailbox del driver al checher
+  trans_fifo_mbx mon_chkr_mbx;           //mailbox del monitor al checher
   trans_sb_mbx chkr_sb_mbx;              //mailbox del checker al scoreboard
   comando_test_sb_mbx test_sb_mbx;       //mailbox del test al scoreboard
   comando_test_agent_mbx test_agent_mbx; //mailbox del test al agente
 
   function new();
     // Instanciación de los mailboxes
-    drv_chkr_mbx   = new();
+    mon_chkr_mbx   = new();
     agnt_drv_mbx   = new();
     chkr_sb_mbx    = new();
-    test_sb_mbx    = new();
-    test_agent_mbx = new();
+    
+    ///test_sb_mbx    = new(); Los quite porque esto lo controla el test no el ambiente
+    ///test_agent_mbx = new();
 
     // instanciación de los componentes del ambiente
     driver_inst     = new();
+    monitor_inst     = new();
     checker_inst    = new();
     scoreboard_inst = new();
     agent_inst      = new();
     // conexion de las interfaces y mailboxes en el ambiente
-    driver_inst.vif             = _if;
-    driver_inst.drv_chkr_mbx    = drv_chkr_mbx;
+    ///driver_inst.vif             = _if;
+    ///monitor_inst.vif             = _if; //agregando esta instancia para el monitor
+    monitor_inst.mon_chkr_mbx    = mon_chkr_mbx;
     driver_inst.agnt_drv_mbx    = agnt_drv_mbx;
-    checker_inst.drv_chkr_mbx   = drv_chkr_mbx;
+    checker_inst.mon_chkr_mbx   = mon_chkr_mbx;
     checker_inst.chkr_sb_mbx    = chkr_sb_mbx;
     scoreboard_inst.chkr_sb_mbx = chkr_sb_mbx;
-    scoreboard_inst.test_sb_mbx = test_sb_mbx;
-    agent_inst.test_agent_mbx   = test_agent_mbx;
+    ///scoreboard_inst.test_sb_mbx = test_sb_mbx;
+    ///agent_inst.test_agent_mbx   = test_agent_mbx;
     agent_inst.agnt_drv_mbx = agnt_drv_mbx;
   endfunction
 
   virtual task run();
     $display("[%g]  El ambiente fue inicializado",$time);
+
+    driver_inst.vif = _if;
+    monitor_inst.vif = _if;
+
+    scoreboard_inst.test_sb_mbx = test_sb_mbx;
+    agent_inst.test_agent_mbx   = test_agent_mbx;
+
+
     fork
       driver_inst.run();
+      monitor_inst.run();
       checker_inst.run();
       scoreboard_inst.run();
       agent_inst.run();
     join_none
   endtask 
 endclass
+
